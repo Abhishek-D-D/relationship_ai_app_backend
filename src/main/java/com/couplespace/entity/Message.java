96412057aa815cb1b173ca1859e4c03d2eea9a57
abhicrezam <sitewpdev@gmail.com>
2026-03-12 20:25:00 +0530

    message data type issue

--- src/main/java/com/couplespace/entity/Message.java
+++ src/main/java/com/couplespace/entity/Message.java
@@ -3,17 +3,24 @@ package com.couplespace.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcType;
+import org.hibernate.dialect.PostgreSQLEnumJdbcType;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
 @Table(name = "messages")
-@Getter @Setter @Builder
-@NoArgsConstructor @AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Message {
 
-    public enum MessageType { TEXT, IMAGE, AUDIO, VIDEO, STICKER }
+    public enum MessageType {
+        TEXT, IMAGE, AUDIO, VIDEO, STICKER
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -27,6 +34,7 @@ public class Message {
     private UUID senderId;
 
     @Enumerated(EnumType.STRING)
+    @JdbcType(PostgreSQLEnumJdbcType.class)
     @Column(name = "message_type", nullable = false)
     @Builder.Default
     private MessageType messageType = MessageType.TEXT;
